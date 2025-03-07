d34028c88f69bf245711f9e55af13b9a21070d56
Simon <simon21.blum@gmail.com>
2025-03-07 11:30:54 +0100

    Split project from labs

--- project/src/controllers/auth/logoutController.js
+++
@@ -1,7 +0,0 @@
-export const logoutController = {
-  handlePostRequest: async (req, res) => {
-    //! TODO ErrorHandling
-    req.session.destroy();
-    res.end("User logged out successfully");
-  },
-};
--- project/src/middleware/pug.js
+++
@@ -1,9 +0,0 @@
-export const pugMiddleware = (req, res, next) => {
-  res.locals.session = req.session;
-  if (req.query.next) {
-    res.locals.originalUrl = req.query.next;
-  } else {
-    res.locals.originalUrl = req.originalUrl;
-  }
-  next();
-};
